14d9ee4690c4510efaea1d7cf56f9242bace89fa
kakashi1210 <desmondaditya@gmail.com>
2023-11-24 02:09:11 +0530

    feat: last push

--- src/components/uploadthing-example.tsx
+++ src/components/uploadthing-example.tsx
@@ -8,7 +8,7 @@ export default function Uploadthing() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<UploadButton
-				endpoint="imageUploader"
+				endpoint="pdfUploader"
 				onClientUploadComplete={(res) => {
 					// Do something with the response
 					console.log("Files: ", res);
