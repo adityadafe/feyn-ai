c3f3b7e4aa9c02188a1a9716de4a8e0e6e3f3736
kakashi1210 <desmondaditya@gmail.com>
2023-12-13 02:36:56 +0530

    feat: added really cool interactive cubes

--- src/components/hero-component.tsx
+++ src/components/hero-component.tsx
@@ -4,8 +4,8 @@ import Spline from '@splinetool/react-spline'
 export default function HeroComponent() {
 	// lg:scale-50 scale-50
 	return (
-		<div className='lg:h-[67vh] lg:w-[25vw] h-[60vh] w-[100vw]  lg:scale-100 scale-100'>
-			<Spline scene='/spline/chips.spline'
+		<div className='lg:h-[67vh] lg:w-[25vw] h-[30vh] w-[100vw] lg:mb-[20vh]  lg:scale-50 scale-[0.2] mb-[20vh] mr-[17vh]'>
+			<Spline scene='/spline/cubic.spline'
 			/>
 		</div>
 	)
