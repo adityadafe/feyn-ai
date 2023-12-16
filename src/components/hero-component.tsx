87faa8ece2581d68ebedfccb0d9618afe0747124
kakashi1210 <desmondaditya@gmail.com>
2023-12-16 21:19:50 +0530

    feat: upload is uploading

--- src/components/hero-component.tsx
+++ src/components/hero-component.tsx
@@ -4,7 +4,7 @@ import Spline from '@splinetool/react-spline'
 export default function HeroComponent() {
 	// lg:scale-50 scale-50
 	return (
-		<div className='lg:h-[67vh] lg:w-[25vw] h-[30vh] w-[100vw] lg:mb-[20vh]  lg:scale-50 scale-[0.2] mb-[20vh] mr-[17vh]'>
+		<div className='lg:h-[67vh] lg:w-[25vw] h-[30vh] w-[100vw] lg:mb-[22vh]  lg:scale-50 scale-[0.2] mb-[20vh] mr-[17vh]'>
 			<Spline scene='/spline/cubic.spline'
 			/>
 		</div>
